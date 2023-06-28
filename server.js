0b447b658fbcdb799fe75954d453fc56eb751ffd
Victor Vermeulen <vcvermeulen@gmail.com>
2023-06-28 19:19:49 +0200

    implement cluster module

--- server.js
+++ server.js
@@ -1,32 +1,40 @@
 const express = require('express');
+const cluster = require('cluster');
 
 const app = express();
 
 function delay(ms) {
-	//const startTime = Date.now();
-	//while (Date.now() - startTime < ms) {
-	// event loop is blocked
-	//}
-	//return;
+	const startTime = Date.now();
+	while (Date.now() - startTime < ms) {
+		// event loop is blocked
+	}
+	return;
 
-	return new Promise((resolve, reject) => {
-		setTimeout(() => {
-			resolve();
-		}, ms);
-	});
+	// return new Promise((resolve, reject) => {
+	// 	setTimeout(() => {
+	// 		resolve();
+	// 	}, ms);
+	// });
 }
 
 app.get('/', (req, res) => {
-	res.send('Performance example');
+	res.send(`Performance example ${process.pid}`);
 });
 
 app.get('/timer', async (req, res) => {
 	// delay the response for 2 seconds
-	await delay(10000);
+	delay(10000);
 
-	res.send('ding ding ding');
+	res.send(`ding ding ding ${process.pid}`);
 });
 
-app.listen(3000, () => {
-	console.log('Server is running on port 3000');
-});
+if (cluster.isMaster) {
+	// Count the machine's CPUs
+	cluster.fork();
+	cluster.fork();
+} else {
+	console.log('Worker process started');
+	app.listen(3000, () => {
+		console.log('Server is running on port 3000');
+	});
+}
